c9a5c050fc1999ef823ede36c9df438b62fee2c5
kd-data-code <kd-data-code>
2024-12-19 20:28:02 +0000

    initial push

---
+++ Spark_Pivot.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+#Create spark session
+data = [("Banana",1000,"USA"), ("Carrots",1500,"USA"), ("Beans",1600,"USA"), \
+      ("Orange",2000,"USA"),("Orange",2000,"USA"),("Banana",400,"China"), \
+      ("Carrots",1200,"China"),("Beans",1500,"China"),("Orange",4000,"China"), \
+      ("Banana",2000,"Canada"),("Carrots",2000,"Canada"),("Beans",2000,"Mexico")]
+
+columns= ["Product","Amount","Country"]
+df = spark.createDataFrame(data = data, schema = columns)
+df.printSchema()
+df.show(truncate=False)
+
+# COMMAND ----------
+
+pivotDF = df.groupBy("Product").pivot("Country").sum("Amount")
+pivotDF.printSchema()
+pivotDF.show(truncate=False)
